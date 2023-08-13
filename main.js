887f55155dd9b2eaedf8215371f2d406ef809616
chunglijung <andy28572463@gmail.com>
2023-08-13 19:42:35 +0800

    feat: 新增sideBarMenu

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-import './assets/scss/all.scss';
+import "./assets/scss/all.scss";
+import { Offcanvas } from "bootstrap";
 
+const offcanvasElementList = document.querySelectorAll(".offcanvas");
+const offcanvasList = [...offcanvasElementList].map(
+  (offcanvasEl) => new bootstrap.Offcanvas(offcanvasEl)
+);
 console.log("Hello world!");
