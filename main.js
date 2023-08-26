3eaf429c14b610f33c9e3b1feaf494e6c6c483b9
chunglijung <andy28572463@gmail.com>
2023-08-26 14:41:23 +0800

    feat: 判斷當前網址讓選單亮色

--- main.js
+++ main.js
@@ -5,4 +5,19 @@ const offcanvasElementList = document.querySelectorAll(".offcanvas");
 const offcanvasList = [...offcanvasElementList].map(
   (offcanvasEl) => new Offcanvas(offcanvasEl)
 );
+
+// init
+document.addEventListener("DOMContentLoaded", function () {
+  // 當前網址
+  const currentUrl = window.location.href;
+  // 選取sideBarMenu 裡所有的 li
+  const sideBarMenuLi = document.querySelectorAll("#sideBarMenu > ul > li");
+  sideBarMenuLi.forEach((li) => {
+    if (li.children[1].href === currentUrl) {
+      li.classList.add("active");
+      li.children[1].classList.remove("text-black");
+      li.children[1].classList.add("text-success");
+    }
+  });
+});
 console.log("Hello world!");
